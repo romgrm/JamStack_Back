914916de221fd1cfbed72bab6045e7723f851954
Romain <romaingreaume@gmail.com>
2021-04-14 11:16:38 +0200

    Back part final

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b78c0dc04e4834d430778f289b6982f6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0d8ffafc-ea2c-4269-b4d6-42e8493e0338'
+};
